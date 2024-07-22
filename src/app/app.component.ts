03818bf36b5f97021d83b4cb4b33dfa59a4aaebe
Yahia952004 <yahiamashaal@gmail.com>
2024-07-22 23:07:13 +0300

    Portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NameComponent } from "./name/name.component";
+import { AcademicYearComponent } from "./academic-year/academic-year.component";
+import { GoalsComponent } from "./goals/goals.component";
+import { HobbiesComponent } from "./hobbies/hobbies.component";
+import { TechnologiesComponent } from "./technologies/technologies.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NameComponent, AcademicYearComponent, GoalsComponent, HobbiesComponent, TechnologiesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
